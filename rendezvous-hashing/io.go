4d26ed439c6c650156ab635bda1b79282ba9a4e5
Michael Mokrysz <hi@46b.it>
2021-09-12 04:48:53 +0100

    Balance read and write availability
    As described in the Markdown doc included in this commit, the system
    now requires success from >50% of replicas for both writes and reads.
    This is much more balanced than the 100% of writes and 1 replica for
    reads previously used.
    
    Code is awful. Sorry.

--- rendezvous-hashing/io.go
+++ rendezvous-hashing/io.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync/atomic"
 
 	"golang.org/x/sync/errgroup"
 	"gopkg.in/yaml.v2"
@@ -58,20 +59,54 @@ func Read(key string, cluster *Cluster) (*Entry, error) {
 		return nil, fmt.Errorf("no nodes found for key")
 	}
 
-	var entry *Entry
-	var err error
+	// Try to read from all replicas; succeed if more than half respond the same
+	// FIXME: Implement conflict resolution so they don't have to respond the same
+	entries := make(chan *Entry, len(chosenReplicas))
+	g := new(errgroup.Group)
 	for _, chosenReplica := range chosenReplicas {
-		entry, err = readEntryFromNode(key, chosenReplica.Node)
-		if err == nil && entry != nil {
-			break
+		node := chosenReplica.Node
+		g.Go(func() error {
+			entry, err := readEntryFromNode(key, node)
+			if entry != nil {
+				entries <- entry
+			}
+			return err
+		})
+	}
+	err := g.Wait()
+	if err != nil {
+		log.Println(fmt.Errorf("error while reading from all replicas: %w", err))
+	}
+	close(entries)
+
+	// FIXME: Go may be a painful language sometimes but there is surely
+	// something I can do to improve this code
+	valuesSeen := map[string]int{}
+	for entry := range entries {
+		if _, ok := valuesSeen[entry.Value]; !ok {
+			valuesSeen[entry.Value] = 1
+		} else {
+			valuesSeen[entry.Value] += 1
 		}
 	}
-
-	if err != nil {
-		// FIXME: Return all errors not just final one?
-		return nil, fmt.Errorf("read failed from all replicas, last error was: %w", err)
+	if len(valuesSeen) == 0 {
+		return nil, err
 	}
-	return entry, err
+	maxTimesSeen := 0
+	maxSeenValue := ""
+	for valueSeen, timesSeen := range valuesSeen {
+		if timesSeen > maxTimesSeen {
+			maxTimesSeen = timesSeen
+			maxSeenValue = valueSeen
+		}
+	}
+	if maxTimesSeen > len(chosenReplicas)/2 {
+		return &Entry{
+			Key:   key,
+			Value: maxSeenValue,
+		}, nil
+	}
+	return nil, fmt.Errorf("failed to read same value from enough replicas (only %d of %d)", maxTimesSeen, len(chosenReplicas))
 }
 
 func readEntryFromNode(key string, node *NodeDescription) (*Entry, error) {
@@ -130,17 +165,26 @@ func Write(entry Entry, cluster *Cluster) error {
 		return fmt.Errorf("no nodes found to accept key")
 	}
 
+	successfulWrites := uint64(0)
 	g := new(errgroup.Group)
 	for _, chosenReplica := range chosenReplicas {
 		node := chosenReplica.Node
 		g.Go(func() error {
-			return writeEntryToNode(entry, node)
+			err := writeEntryToNode(entry, node)
+			if err == nil {
+				atomic.AddUint64(&successfulWrites, 1)
+			}
+			return err
 		})
 	}
-	if err := g.Wait(); err != nil {
-		return fmt.Errorf("error writing to all replicas: %w", err)
+	err := g.Wait()
+	if successfulWrites > uint64(len(chosenReplicas)/2) {
+		if err != nil {
+			log.Println(err)
+		}
+		return nil
 	}
-	return nil
+	return err
 }
 
 func writeEntryToNode(entry Entry, node *NodeDescription) error {
